f26fc3cf21083e148200b5e440132ce4f22c4fee
Fathan <74785057+Churma16@users.noreply.github.com>
2023-04-03 22:46:59 +0700

    fixing navbar

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -9,29 +9,26 @@
                             new Date().getFullYear()
                         );
                     </script>
-                    , made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative
-                        Tim</a>
-                    for a better web.
+                    , made with <i class="fa fa-heart"></i>
                 </div>
             </div>
             <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
                         <a href="https://www.creative-tim.com" class="nav-link text-muted"
-                            target="_blank">Creative Tim</a>
+                            target="_blank">Kota Semarang</a>
                     </li>
                     <li class="nav-item">
                         <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted"
-                            target="_blank">About Us</a>
+                            target="_blank">Diskominfo Kota Semarang</a>
                     </li>
                     <li class="nav-item">
                         <a href="https://www.creative-tim.com/blog" class="nav-link text-muted"
-                            target="_blank">Blog</a>
+                            target="_blank">About Us</a>
                     </li>
                     <li class="nav-item">
                         <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted"
-                            target="_blank">License</a>
+                            target="_blank"></a>
                     </li>
                 </ul>
             </div>
--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -23,18 +23,9 @@
                         <span class="d-sm-inline d-none">{{ auth()->user()->name }}</span>
                     </a>
                 </li>
-                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-                    <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-                        <div class="sidenav-toggler-inner">
-                            <i class="sidenav-toggler-line"></i>
-                            <i class="sidenav-toggler-line"></i>
-                            <i class="sidenav-toggler-line"></i>
-                        </div>
-                    </a>
-                </li>
                 <li class="nav-item px-3 d-flex align-items-center">
-                    <a href="javascript:;" class="nav-link text-body p-0">
-                        <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
+                    <a href="/logout" class="nav-link text-body p-0">
+                        <i class="fas fa-sign-out-alt fixed-plugin-button-nav cursor-pointer"></i> <b>Log Out</b>
                     </a>
                 </li>
                 <li class="nav-item dropdown pe-2 d-flex align-items-center">
