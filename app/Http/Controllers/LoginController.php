5988e204d5eccb1426973bdd19784eef4f54892b
Fathan <74785057+Churma16@users.noreply.github.com>
2023-05-28 04:15:08 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,29 +7,53 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index(){
+    /**
+     * Display the login form.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
         return view('login.index');
     }
 
-    public function authenticate(Request $request){
-
+    /**
+     * Authenticate the user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function authenticate(Request $request)
+    {
+        // Validate the form input fields.
         $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        if(Auth::attempt($credentials)){
+        // Attempt to authenticate the user using the provided credentials.
+        if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
+            // Redirect the user to the intended page or dashboard.
             return redirect()->intended('/dashboard');
         }
 
+        // If authentication fails, redirect back to the login form with an error message.
         return back()->with('loginError', 'Email atau password yang dimasukan salah!');
-
     }
 
-    public function logout(){
+    /**
+     * Logout the authenticated user.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function logout()
+    {
+        // Logout the authenticated user.
         Auth::logout();
+
+        // Redirect the user to the login page.
         return redirect('/login');
     }
 }
